eb9a5735ee5bde55961d2c119312bf6846351344
Vladimir Stolyarov <xakep6666@gmail.com>
2016-11-29 01:37:24 +0300

    add expose annotation to class fields in protocol.model

--- src/protocol/model/Food.java
+++ src/protocol/model/Food.java
@@ -1,9 +1,13 @@
 package protocol.model;
 
+import com.google.gson.annotations.Expose;
+
 /**
  * @author apomosov
  */
 public final class Food {
+  @Expose
   private int x;
+  @Expose
   private int y;
 }
