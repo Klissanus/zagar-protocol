369b88ee981cbf0df4468954c1076090816058cc
Vladimir Stolyarov <xakep6666@gmail.com>
2016-12-18 21:36:45 +0300

    Remove default constructor

--- src/protocol/model/Cell.java
+++ src/protocol/model/Cell.java
@@ -13,11 +13,6 @@ public abstract class Cell {
   @Expose
   private final Point2D coordinate;
 
-  protected Cell() {
-    size=0;
-    coordinate=new Point2D.Double(0,0);
-  }
-
   public Cell(float size, Point2D coordinate) {
     this.size = size;
     this.coordinate = coordinate;
