5c3c3043f4467d17328a3de268079689aba6cb50
Vladimir Stolyarov <xakep6666@gmail.com>
2016-12-18 21:59:45 +0300

    Use x,y instead of Point2D

--- src/protocol/model/Cell.java
+++ src/protocol/model/Cell.java
@@ -11,19 +11,21 @@ public abstract class Cell {
   @Expose
   private final float size;
   @Expose
-  private final Point2D coordinate;
+  private final double x,y;
 
   protected Cell() {
     size=0;
-    coordinate=new Point2D.Double(0,0);
+    x=0;
+    y=0;
   }
 
   public Cell(float size, Point2D coordinate) {
     this.size = size;
-    this.coordinate = coordinate;
+    this.x=coordinate.getX();
+    this.y=coordinate.getY();
   }
 
-  public Point2D getCoordinate() {return coordinate;}
+  public Point2D getCoordinate() {return new Point2D.Double(x,y);}
 
   public float getSize() {
     return size;
